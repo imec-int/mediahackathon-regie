cc09dc409e88ac7980fd6ed581db93e1ade033b0
kaosbeat <kasper.jordaens@gmail.com>
2014-03-25 14:40:58 +0100

    hack stats per hack in controller

--- app/public/javascripts/controller.js
+++ app/public/javascripts/controller.js
@@ -29,6 +29,13 @@ var Controller = function (options){
 		});
 		socket.on('stats', function(stats) {
 			$('#connectedphones').html(stats.smartphones)
+			console.log(stats.usersPerHack)
+			$('#eplepticstats').html(stats.usersPerHack.epleptic)
+			$('#phiemelstats').html(stats.usersPerHack.phiemel)
+			$('#hexamusicstats').html(stats.usersPerHack.hexamusic)
+			$('#sounddefenderstats').html(stats.usersPerHack.sounddefender)
+			$('#mediagoostats').html(stats.usersPerHack.mediagoo)
+			$('#oscilloscoopstats').html(stats.usersPerHack.oscilloscoop)
 		});
 	};
 
