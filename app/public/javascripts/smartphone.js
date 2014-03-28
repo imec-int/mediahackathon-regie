95d56bf414ed7dd4a971b2ac43d7cf17e9e92bdd
kaosbeat <kasper.jordaens@gmail.com>
2014-03-28 12:33:27 +0100

    switch to right overlay

--- app/public/javascripts/smartphone.js
+++ app/public/javascripts/smartphone.js
@@ -48,7 +48,7 @@ var Smartphone = function (options){
 		setTimeout(function () {
 			$iframe.attr('src', data.url);
 			$('head>title').text('mixapp.be | ' + data.title);
-			console.log(data);
+			$('#overlayimg').attr('src', '/images/overlay/' +data.id+ '.png')
 			$('#overlay').show();
 		},delay);
 	};
