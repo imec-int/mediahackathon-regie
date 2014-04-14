83e3226e81f4c11c67b0377c394742a44c81d2d2
Sam Decrock <sam.decrock@gmail.com>
2014-04-14 14:50:06 +0200

    updated regie to use local ips

--- app/config.js
+++ app/config.js
@@ -10,7 +10,7 @@ exports.hacks = [
 	{
 		id         : 'epleptic',
 		title      : 'Epleptic',
-		smartphone : 'http://172.16.15.102:7000',
+		smartphone : 'http://10.100.1.1:7000',
 		overlay    : '/images/overlay/epleptic.png',
 		svo        : ['/images/svo/Epleptic_scherm1.png','/images/svo/Epleptic_scherm2.png'],
 		svostate   : 0
@@ -19,7 +19,7 @@ exports.hacks = [
 	{
 		id         : 'phiemel',
 		title      : 'Phiemel',
-		smartphone : 'http://172.16.15.103:3000',
+		smartphone : 'http://10.100.1.1:3001',
 		overlay    : '/images/overlay/phiemel.png',
 		svo        : ['/images/svo/Pale-eyes_scherm1.png','/images/svo/Pale-eyes_scherm2.png'],
 		svostate   : 0
@@ -28,7 +28,7 @@ exports.hacks = [
 	{
 		id         : 'hexamusic',
 		title      : 'Hexamusic',
-		smartphone : 'http://172.16.15.104:3000/smartphone/index2.html',
+		smartphone : 'http://10.100.1.1:3002/smartphone/index2.html',
 		overlay    : '/images/overlay/hexamusic.png',
 		svo        : ['/images/svo/Hexamusic_scherm1.png'],
 		svostate   : 0
@@ -37,7 +37,7 @@ exports.hacks = [
 	{
 		id         : 'sounddefender',
 		title      : 'Sound Defender',
-		smartphone : 'http://172.16.15.105:9080',
+		smartphone : 'http://10.100.1.1:9080',
 		overlay    : '/images/overlay/sounddefender.png',
 		svo        : ['/images/svo/Sound-defender_scherm1.png','/images/svo/Sound-defender_scherm2.png'],
 		svostate   : 0
@@ -46,7 +46,7 @@ exports.hacks = [
 	{
 		id         : 'mediagoo',
 		title      : 'MediaGoo',
-		smartphone : 'http://172.16.15.106:3000',
+		smartphone : 'http://10.100.1.1:3003',
 		overlay    : '/images/overlay/mediagoo.png',
 		svo        : ['/images/svo/Mediagoo_scherm1.png','/images/svo/Mediagoo_scherm2.png'],
 		svostate   : 0
